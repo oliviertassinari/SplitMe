be1a0996de2967fc328823abb89b5eceee39cfc1
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-02-27 16:36:15 +0100

    [expense] fix ui

--- src/Expense/DetailView.jsx
+++ src/Expense/DetailView.jsx
@@ -6,21 +6,39 @@ var Paper = mui.Paper;
 var TextField = mui.TextField;
 var DatePicker = mui.DatePicker;
 var FontIcon = mui.FontIcon;
+var DropDownMenu = mui.DropDownMenu;
 
 var DetailView = React.createClass({
+  componentDidMount: function() {
+    var self = this;
+
+    setTimeout(function(){
+      self.refs.Description.focus();
+    }, 0);
+  },
+
+  formatDate: function(date) {
+    return date;
+  },
+
   render: function () {
     var today = new Date();
 
+    var menuItemsCurrency = [
+       { payload: '€', text: '€' },
+       { payload: '$', text: '$' },
+    ];
+
     return <Paper zDepth={1} innerClassName="expense-detail" rounded={false}>
-      <TextField hintText="Description" /><br />
+      <TextField hintText="Description" ref="Description"/><br />
       <div className="expense-detail-item">
         <FontIcon className="md-local-atm"/>
         <TextField hintText="0.00" type="number"/>
-        €
+        <DropDownMenu menuItems={menuItemsCurrency} />
       </div>
       <div className="expense-detail-item">
         <FontIcon className="md-schedule" />
-        <DatePicker hintText="Date" defaultDate={today} />
+        <DatePicker hintText="Date" defaultDate={today} formatDate={this.formatDate}/>
       </div>
       <div className="expense-detail-item">
         <FontIcon className="md-person" />
