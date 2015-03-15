af8df8ffb0df4898a2dae4cdfaa1b73b8ce716fa
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-03-15 13:09:44 +0100

    [API] add getAccountAll()

--- src/Account/store.jsx
+++ src/Account/store.jsx
@@ -2,55 +2,13 @@
 
 var _ = require('underscore');
 
+var API = require('../API');
 var dispatcher = require('../dispatcher');
 var EventEmitter = require('events').EventEmitter;
-var PouchDB = require('pouchdb');
 
-var db = new PouchDB('split');
 
 var _accounts = [];
 var _accountCurrent = null;
-
-function addExpense(db, name, value) {
-  return db.put({
-    _id: new Date().toJSON(),
-    name: name,
-    dateLastExpense: 'date',
-    members: [{
-      id: '0',
-      displayName: 'Me',
-    },{
-      id: '10',
-      displayName: 'Nicolas',
-    }],
-    balances: [{
-      value: value,
-      currency: 'EUR',
-    }],
-  });
-}
-
-function fetchAll() {
-  return new PouchDB('split').destroy().then(function() {
-    return new PouchDB('split');
-  }).then(function (db) {
-      return addExpense(db, 'Titre 1', -4.30)
-        .then(function() {
-          return addExpense(db, 'Titre 2', 1.13);
-        }).then(function() {
-          return db.allDocs({
-            include_docs: true
-          });
-        }).then(function (result) {
-          var rows = _.map(result.rows, function(row) {
-            return row.doc;
-          });
-          return rows;
-        }).catch(function (err) {
-          console.log(err);
-        });
-  });
-}
 
 var store = _.extend({}, EventEmitter.prototype, {
   getAll: function() {
@@ -76,7 +34,7 @@ var store = _.extend({}, EventEmitter.prototype, {
 dispatcher.register(function(action) {
   switch(action.actionType) {
     case 'ACCOUNT_FETCH_ALL':
-      fetchAll().then(function(result) {
+      API.getAccountAll().then(function(result) {
         _accounts = result;
         store.emitChange();
       });
