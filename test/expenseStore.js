cb7e53613ecf6e4dd5c66e7467a962eca2b6e9b7
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-03-17 23:25:54 +0100

    [test] restart

---
+++ test/expenseStore.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+
+describe('expenseStore', function() {
+  describe('#applyExpenseToAccounts()', function() {
+    it('should return 1 element when we store', function(done) {
+      assert.equal(1, 2);
+    });
+  });
+});
