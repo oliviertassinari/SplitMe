c76a13b7cd3366a387f9487d3b6447d59d00372b
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-03-18 14:12:34 +0100

    [test] utils equaly ok

--- src/utils.jsx
+++ src/utils.jsx
@@ -2,47 +2,62 @@
 
 module.exports = {
 
+  roundAmount: function(amount) {
+    return Math.round(100 * amount) / 100;
+  },
+
   applyExpenseToAccounts: function(expense) {
+    // Apply for each paidFor contact
     for (var i = 0; i < expense.paidFor.length; i++) {
       var paidFor = expense.paidFor[i];
-      var accountToUpdate;
 
-      // console.log('paidFor', paidFor);
+      if(paidFor.contactId !== expense.paidByContactId) {
+        var accountToUpdate;
 
-      for (var j = 0; j < expense.accounts.length; j++) {
-        var account = expense.accounts[j];
-        var foundPaidBy = false;
-        var foundPaidFor = false;
+        // Search account to update
+        for (var j = 0; j < expense.accounts.length; j++) {
+          var account = expense.accounts[j];
+          var foundPaidBy = false;
+          var foundPaidFor = false;
 
-        for (var k = 0; k < account.members.length; k++) {
-          var member = account.members[k];
+          // Search contact in members
+          for (var k = 0; k < account.members.length; k++) {
+            var member = account.members[k];
 
-          if(member.id === expense.paidByContactId) {
-            foundPaidBy = true;
-          } else if(member.id === paidFor.contactId) {
-            foundPaidFor = true;
+            if(member.id === expense.paidByContactId) {
+              foundPaidBy = true;
+            } else if(member.id === paidFor.contactId) {
+              foundPaidFor = true;
+            }
+          }
+
+          if(foundPaidFor && foundPaidBy) {
+            accountToUpdate = account;
+            break;
           }
         }
 
-        if(foundPaidFor && foundPaidBy) {
-          accountToUpdate = account;
-          break;
+        var balanceDiff;
+
+        switch(expense.split) {
+          case 'equaly':
+            balanceDiff = expense.amount / expense.paidFor.length;
+            break;
+
+          case 'unequaly':
+            break;
+
+          case 'shares':
+            break;
         }
 
-        // console.log(foundPaidFor, foundPaidBy);
-      }
+        if(expense.paidByContactId !== '0') {
+          balanceDiff *= -1;
+        }
 
-      // console.log('accountToUpdate', accountToUpdate);
+        accountToUpdate.balances[0].value = balanceDiff;
 
-      switch(expense.split) {
-        case 'equaly':
-          break;
-
-        case 'unequaly':
-          break;
-
-        case 'shares':
-          break;
+        // console.log('accountToUpdate', accountToUpdate.name, balanceDiff);
       }
     }
   },
