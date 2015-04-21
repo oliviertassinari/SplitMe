343625a7f0c012ce6066e94737b35acd33f7faf4
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-04-21 23:36:29 +0200

    [cordova] add Google Analytics

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ var locale = require('./locale');
 var Main = require('./Main/Main');
 var accountAction = require('./Main/Account/action');
 var pageAction = require('./Main/pageAction');
+var pageStore = require('./Main/pageStore');
 
 if (process.env.NODE_ENV !== 'production') {
   window.Perf = React.addons.Perf;
@@ -28,10 +29,16 @@ if (process.env.NODE_ENV !== 'production') {
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-  console.log('onDeviceReady');
-  console.log('contacts', navigator.contacts);
-
   document.addEventListener('backbutton', onBackButton, false);
+
+  var analytics = window.analytics;
+  analytics.startTrackerWithId('UA-44093216-2');
+
+  analytics.trackView(pageStore.get());
+
+  pageStore.addChangeListener(function() {
+    analytics.trackView(pageStore.get());
+  });
 }
 
 function onBackButton() {
