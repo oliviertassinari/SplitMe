f31d2eed394d63e023bc09ca0cddd21c7ee5cb80
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-05-24 14:50:16 +0200

    [Account] start Balance and Debts view

---
+++ src/Main/Account/Balance.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+var React = require('react');
+
+var AccountBalance = React.createClass({
+  propTypes: {
+    members: React.PropTypes.object,
+  },
+  render: function() {
+    return <div></div>;
+  },
+});
+
+module.exports = AccountBalance;
