055be1314fda33bb9f6cd8ea82e92a7521c7b7d5
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-07-16 00:14:36 +0100

    [webpack] use only one config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,65 +4,112 @@ var path = require('path');
 var packageJson = require('./package.json');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8000', // WebpackDevServer
-    'webpack/hot/only-dev-server',
-    './src/app.jsx',
-  ],
-  output: {
-    path: path.join(__dirname, 'build'),
-    publicPath: '',
-    filename: 'app.js',
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    packageMains: ['webpack', 'browser', 'web', 'browserify', 'main'], // remove jam from default
-    alias: {
-      'intl': path.join(__dirname, 'node_modules/intl/Intl.js'),
+module.exports = function(options) {
+  var config = {
+    output: {
+      path: path.join(__dirname, 'cordova/www'), // No used by webpack dev server
+      publicPath: '',
+      filename: 'app.js',
     },
-    root: path.join(__dirname, 'src'),
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.NoErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      alias: {
+        'intl': path.join(__dirname, 'node_modules/intl/Intl.js'),
       },
-    }),
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(packageJson.version),
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  module: {
-    noParse: /lie.js/,
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['react-hot-loader', 'jsx-loader?harmony'],
-      },
-      {
-        test: /\.less?$/,
-        loaders: [
-          'style-loader',
-          'css-loader',
-          'autoprefixer-loader?{browsers:["last 2 versions"]}',
-          'less-loader'
-        ],
-      },
-      {
-        test: /\.woff?$/,
-        loader: 'url-loader?limit=100000',
-      },
+      root: path.join(__dirname, 'src'),
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new webpack.NoErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src/index.html'),
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+      }),
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(packageJson.version),
+        'process.env': {
+          NODE_ENV: JSON.stringify(options.environment),
+        },
+      }),
     ],
-  },
-  devtool: 'eval',
+    module: {
+      noParse: /lie\.js$|\/levelup\//,
+    },
+    devtool: (options.environment === 'development') ? 'eval' : null,
+  };
+
+  if (options.environment === 'development') {
+    config.entry = [
+        'webpack-dev-server/client?http://0.0.0.0:8000', // WebpackDevServer
+        'webpack/hot/only-dev-server',
+        './src/app.jsx',
+      ];
+
+    config.plugins = config.plugins.concat([
+      new webpack.HotModuleReplacementPlugin(),
+    ]);
+
+    config.module.loaders = [
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot-loader', 'jsx-loader?harmony'],
+        },
+        {
+          test: /\.less?$/,
+          loaders: [
+            'style-loader',
+            'css-loader',
+            'autoprefixer-loader?{browsers:["last 2 versions"]}',
+            'less-loader'
+          ],
+        },
+        {
+          test: /\.woff?$/,
+          loader: 'url-loader?limit=100000',
+        },
+      ];
+  } else if (options.environment === 'production') {
+    config.entry = [
+        './src/app.jsx'
+      ];
+
+    config.plugins = config.plugins.concat([
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          compressor: {
+            warnings: false,
+          },
+          output: {
+            comments: false,
+          },
+        }),
+        new ExtractTextPlugin('app.css'),
+      ]);
+
+    config.module.loaders = [
+        {
+          test: /\.jsx?$/,
+          loaders: ['jsx-loader?harmony'],
+        },
+        {
+          test: /\.less?$/,
+          loader: ExtractTextPlugin.extract(
+            'style-loader',
+            'css-loader!autoprefixer-loader?{browsers:["last 2 versions"]}!less-loader'
+          ),
+        },
+        {
+          test: /\.woff?$/,
+          loader: 'url-loader?limit=100000',
+        },
+      ];
+  }
+
+  return config;
 };
