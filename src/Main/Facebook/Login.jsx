4e61cb683e8588d8ace74d5eea5909a468b7e9a3
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-07-23 19:07:11 +0100

    [FacebookLogin] new component

---
+++ src/Main/Facebook/Login.jsx
@@ -0,0 +1,46 @@
+'use strict';
+
+var React = require('react');
+var FlatButton = require('material-ui/lib/flat-button');
+var colors = require('material-ui/lib/styles/colors');
+
+var polyglot = require('polyglot');
+var facebookAction = require('Main/Facebook/action');
+
+var styles = {
+  facebookEmail: {
+    color: colors.grey600,
+  },
+};
+
+var FacebookLogin = React.createClass({
+  propTypes: {
+    facebook: React.PropTypes.object.isRequired,
+  },
+  onTouchTapLogin: function() {
+    facebookAction.login();
+  },
+  render: function() {
+    var facebook = this.props.facebook;
+
+    if (facebook.get('status') === 'connected') {
+      var email;
+
+      if (facebook.get('me')) {
+        email = <div style={styles.facebookEmail}>
+            {'(' + facebook.getIn(['me', 'email']) + ')'}
+          </div>;
+      }
+
+      return <div>
+          <div>{polyglot.t('facebook_you_are_logged')}</div>
+          {email}
+        </div>;
+    } else {
+      return <FlatButton label={polyglot.t('facebook_login')}
+        onTouchTap={this.onTouchTapLogin} />;
+    }
+  },
+});
+
+module.exports = FacebookLogin;
--- src/Main/Settings/Settings.jsx
+++ src/Main/Settings/Settings.jsx
@@ -7,19 +7,11 @@ var Paper = require('material-ui/lib/paper');
 var IconButton = require('material-ui/lib/icon-button');
 var IconClose = require('material-ui/lib/svg-icons/navigation/close');
 var ListItem = require('material-ui/lib/lists/list-item');
-var FlatButton = require('material-ui/lib/flat-button');
-var colors = require('material-ui/lib/styles/colors');
 var EventListener = require('react-event-listener');
 
 var polyglot = require('polyglot');
 var pageAction = require('Main/pageAction');
-var facebookAction = require('Main/Facebook/action');
-
-var styles = {
-  facebookEmail: {
-    color: colors.grey600,
-  },
-};
+var FacebookLogin = require('Main/Facebook/Login');
 
 var Settings = React.createClass({
   propTypes: {
@@ -40,36 +32,10 @@ var Settings = React.createClass({
     event.preventDefault();
     pageAction.navigateHome();
   },
-  onTouchTapFacebook: function() {
-    facebookAction.login();
-  },
   render: function() {
-    var facebook = this.props.facebook;
-
     var appBarLeft = <IconButton onTouchTap={this.onTouchTapClose}>
         <IconClose />
       </IconButton>;
-
-    var listItemFacebook;
-
-    if (facebook.get('status') === 'connected') {
-      var email;
-
-      if (facebook.get('me')) {
-        email = <div style={styles.facebookEmail}>
-            {'(' + facebook.getIn(['me', 'email']) + ')'}
-          </div>;
-      }
-
-      listItemFacebook = <div>
-          <div>{polyglot.t('facebook_you_are_logged')}</div>
-          {email}
-        </div>;
-    } else {
-      listItemFacebook = <FlatButton label={polyglot.t('facebook_login')} onTouchTap={this.onTouchTapFacebook} />;
-    }
-
-    console.log(facebook);
 
     return <AppCanvas>
       <AppBar title={polyglot.t('settings')}
@@ -81,7 +47,7 @@ var Settings = React.createClass({
             {polyglot.t('version') + ' ' + VERSION}
           </ListItem>
           <ListItem disabled={true}>
-            {listItemFacebook}
+            <FacebookLogin facebook={this.props.facebook} />
           </ListItem>
         </Paper>
       </div>
