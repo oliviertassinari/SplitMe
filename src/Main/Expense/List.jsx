c7ccacd6d254c2ce0d564f71eb8e8ab461c579f9
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-08-17 21:00:52 +0100

    [ExpenseList] use infinit list to speedup rendering

--- src/Main/Expense/List.jsx
+++ src/Main/Expense/List.jsx
@@ -5,6 +5,7 @@ var Immutable = require('immutable');
 var moment = require('moment');
 var Paper = require('material-ui/lib/paper');
 var colors = require('material-ui/lib/styles/colors');
+var ReactList = require('react-list');
 
 var polyglot = require('polyglot');
 var utils = require('utils');
@@ -29,43 +30,60 @@ var ExpenseList = React.createClass({
   mixins: [
     React.addons.PureRenderMixin,
   ],
+  getInitialState: function() {
+    return {
+      expensesSorted: this.getExpensesSorted(this.props.account.get('expenses')),
+    };
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      expensesSorted: this.getExpensesSorted(nextProps.account.get('expenses')),
+    })
+  },
+  getExpensesSorted: function(expenses) {
+    // Can't sort
+    if (!API.isExpensesFetched(expenses)) {
+      return expenses;
+    }
+
+    // DESC date order
+    return expenses.sort(function(expenseA, expenseB) {
+      return expenseA.get('date') < expenseB.get('date');
+    });
+  },
   onTouchTapList: function(expense, event) {
     event.preventDefault();
     action.tapList(expense);
   },
-  render: function() {
-    var self = this;
+  renderItem: function(index, key) {
     var account = this.props.account;
-    var expenses = account.get('expenses');
+    var expense = this.state.expensesSorted.get(index);
+
+    var amount = new locale.intl.NumberFormat(locale.current, {
+      style: 'currency',
+      currency: expense.get('currency'),
+    }).format(expense.get('amount'));
+    var paidBy = utils.getAccountMember(account, expense.get('paidByContactId'))[1];
+    var date = moment(expense.get('date'), 'YYYY-MM-DD').format('ll');
+    var avatar = <MemberAvatar member={paidBy} />;
+
+    return <List key={key} left={avatar} right={amount} onTouchTap={this.onTouchTapList.bind(this, expense)}>
+        {expense.get('description')}
+        <div style={styles.description}>
+          {polyglot.t('paid_by_name', {name: utils.getNameMember(paidBy)}) + ', ' + date}
+        </div>
+      </List>;
+  },
+  render: function() {
+    var expenses = this.props.account.get('expenses');
 
     // Wait loading for expenses
     if (!API.isExpensesFetched(expenses)) {
       return <div />;
     }
 
-    // DESC date order
-    expenses = expenses.sort(function(expenseA, expenseB) {
-      return expenseA.get('date') < expenseB.get('date');
-    });
-
     return <Paper rounded={false}>
-        {expenses.map(function(expense) {
-          var amount = new locale.intl.NumberFormat(locale.current, {
-            style: 'currency',
-            currency: expense.get('currency'),
-          }).format(expense.get('amount'));
-          var paidBy = utils.getAccountMember(account, expense.get('paidByContactId'))[1];
-          var date = moment(expense.get('date'), 'YYYY-MM-DD').format('ll');
-          var avatar = <MemberAvatar member={paidBy} />;
-
-          return <List key={expense.get('_id')} left={avatar} right={amount}
-                  onTouchTap={self.onTouchTapList.bind(self, expense)}>
-              {expense.get('description')}
-              <div style={styles.description}>
-                {polyglot.t('paid_by_name', {name: utils.getNameMember(paidBy)}) + ', ' + date}
-              </div>
-            </List>;
-        })}
+        <ReactList itemRenderer={this.renderItem} length={expenses.size} type="uniform" />
       </Paper>;
   },
 });
