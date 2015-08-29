2f38548484f9fbb1963b8096cbd4487fa5359d4d
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-08-29 01:32:55 +0100

    [crash] add action try catch

--- src/analyticsTraker.js
+++ src/analyticsTraker.js
@@ -10,7 +10,7 @@ function getPageCurrent() {
   return store.getState().getIn(['screen', 'page']);
 }
 
-var analyticsTracker = {
+var analyticsTraker = {
   onDeviceReady: function() {
     var analytics = window.analytics; // Not defined before the onDeviceReady event
 
@@ -35,6 +35,21 @@ var analyticsTracker = {
   },
 };
 
-module.exports = function() {
-  document.addEventListener('deviceready', analyticsTracker.onDeviceReady, false);
+module.exports = {
+  crashReporter: function() {
+    return function(next) {
+      return function(action) {
+        try {
+          return next(action);
+        } catch(err) {
+          console.error('Caught an exception!', err);
+          window.analytics.trackException('action ' + action.type + ' - ' + err, true); // (Description, Fatal)
+          throw err;
+        }
+      };
+    };
+  },
+  load: function() {
+    document.addEventListener('deviceready', analyticsTraker.onDeviceReady, false);
+  },
 };
--- src/app.jsx
+++ src/app.jsx
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'development') {
   };
 }
 
-analyticsTraker(); // Load
+analyticsTraker.load();
 injectTapEventPlugin();
 
 locale.load()
