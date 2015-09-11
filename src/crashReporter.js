ce5b9fd3cd32c4328041f392a151b0d2a3f9f10e
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-09-11 19:32:17 +0100

    [eslint] upgrade

--- src/crashReporter.js
+++ src/crashReporter.js
@@ -5,7 +5,7 @@ function crashReporter() {
     return function(action) {
       try {
         return next(action);
-      } catch(err) {
+      } catch (err) {
         console.error('Caught an exception!', err);
         window.analytics.trackException('action ' + action.type + ' - ' + err, true); // (Description, Fatal)
         throw err;
