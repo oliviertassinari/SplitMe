7ab65c4b76d88ae66900f380ab6880a09a32efa9
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-09-21 23:50:27 +0200

    [Analytics] improve error reporting

--- src/crashReporter.js
+++ src/crashReporter.js
@@ -6,9 +6,12 @@ function crashReporter() {
       try {
         return next(action);
       } catch (err) {
+        /**
+         * Android
+         * https://developers.google.com/analytics/devguides/collection/android/v4/exceptions
+         */
+        window.analytics.trackException(action.type + ' - ' + err, true); // (Description up to 100, Fatal)
         console.error('Caught an exception!', err);
-        window.analytics.trackException('action ' + action.type + ' - ' + err, true); // (Description, Fatal)
-        throw err;
       }
     };
   };
