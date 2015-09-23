a5c797248238386b6c6fdd21535e1452b505ffc5
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-09-23 17:45:15 +0200

    [eslint] use the autofix option

--- src/app.jsx
+++ src/app.jsx
@@ -40,7 +40,7 @@ locale.load()
   .then(function() {
     React.render(
       <Provider store={store}>
-        {function() { // To remove with react v0.14
+        {() => { // To remove with react v0.14
           return <Main />;
         }}
       </Provider>,
