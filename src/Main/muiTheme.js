77081716ec063ac856083fad1bd9a36202e7a189
Wercker CI <olivier.tassinari@gmail.com>
2015-12-04 11:11:21 +0100

    [theme] Use cameCase for object

--- src/Main/muiTheme.js
+++ src/Main/muiTheme.js
@@ -1,17 +1,17 @@
-import Colors from 'material-ui/lib/styles/colors';
-import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import colors from 'material-ui/lib/styles/colors';
+import themeManager from 'material-ui/lib/styles/theme-manager';
 
 import defaultRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
 defaultRawTheme.palette = Object.assign({}, defaultRawTheme.palette, {
-  primary1Color: Colors.green500,
-  primary2Color: Colors.green700,
-  primary3Color: Colors.green100,
-  accent1Color: Colors.red500,
+  primary1Color: colors.green500,
+  primary2Color: colors.green700,
+  primary3Color: colors.green100,
+  accent1Color: colors.red500,
 });
 
-const muiTheme = ThemeManager.getMuiTheme(defaultRawTheme);
+const muiTheme = themeManager.getMuiTheme(defaultRawTheme);
 muiTheme.appBar = Object.assign({}, muiTheme.appBar, {
-  textColor: Colors.white,
+  textColor: colors.white,
   height: 56,
 });
 muiTheme.avatar = Object.assign({}, muiTheme.avatar, {
