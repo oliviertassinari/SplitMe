46f1c891a98f1f6d98bc7b2ba895c5d203100b04
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-10 19:03:23 +0100

    [browser] Add service worker

---
+++ src/serviceWorker.js
@@ -0,0 +1,30 @@
+import registerServiceWorker from 'serviceworker!./sw.js';
+
+export default {
+  install() {
+    if (navigator.serviceWorker && window.location.protocol === 'https:') {
+      registerServiceWorker({
+        scope: '/',
+      }).then((registration) => {
+        console.log('ServiceWorker registration successful');
+        console.log(registration);
+        registration.addEventListener('updatefound', () => {
+          const newWorker = registration.installing;
+
+          registration.installing.addEventListener('statechange', () => {
+            if (newWorker.state === 'activated' && !navigator.serviceWorker.controller) {
+              // the very first activation!
+              // tell the user stuff works offline
+              console.log('sw onFirstLoad, ready to work offline');
+            }
+
+            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              // We could ask the user to reload.
+              console.log('sw onInstalled');
+            }
+          });
+        });
+      });
+    }
+  },
+};
---
+++ src/sw.js
@@ -0,0 +1,69 @@
+/**
+ * When the user navigates to your site,
+ * the browser tries to redownload the script file that defined the service worker in the background.
+ * If there is even a byte's difference in the service worker file compared to what it currently has,
+ * it considers it 'new'.
+ */
+
+const CACHE_NAME = 'cache-v';
+const urlsToCache = [
+  '/',
+];
+
+// install: when the service worker is first added to a computer
+self.addEventListener('install', (event) => {
+  // Perform install steps
+
+  event.waitUntil(() => {
+    //Add core website files to cache during serviceworker installation
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    });
+  });
+});
+
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  // This service worker won't touch non-get requests
+  if (request.method !== 'GET') {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request)
+      .then((response) => {
+        if (response) {
+          // Cache hit - return response
+          return response;
+        }
+
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then((response2) => {
+          // Check if we received a valid response2
+          // Basic means from the origin
+          if (!response2 || response2.status !== 200 || response2.type !== 'basic') {
+            return response2;
+          }
+
+          const responseToCache = response2.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response2;
+        });
+      }
+    )
+  );
+});
+
+// After the install event
+self.addEventListener('activate', () => {
+
+});
