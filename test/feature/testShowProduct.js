13d0300b16ac055342d8127302044bc86d9180b6
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-01-02 21:45:37 +0100

    [Product] Add test

---
+++ test/feature/testShowProduct.js
@@ -0,0 +1,22 @@
+import {assert} from 'chai';
+
+import fixture from '../fixture';
+
+describe('product', () => {
+  before((done) => {
+    browser
+      .url('http://0.0.0.0:8000/?locale=fr')
+      .timeoutsAsyncScript(5000)
+      .executeAsync(fixture.executeAsyncDestroyAll) // node.js context
+      .call(done);
+  });
+
+  it('should show product when we navigate to the home page', (done) => {
+    browser
+      .getText('[data-test=AppBar] h1', (err, text) => {
+        assert.equal(text, 'SplitMe');
+      })
+      .call(done);
+  });
+
+});
