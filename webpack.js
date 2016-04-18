21532ff5e413c68d011912cfc46d1c7bc63d4d48
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-04-18 21:40:20 +0200

    [Wercker] Remove grunt-cli

--- webpack.js
+++ webpack.js
@@ -5,100 +5,106 @@ import webpackConfig from './webpack.config';
 import ProgressPlugin from 'webpack/lib/ProgressPlugin';
 import rimraf from 'rimraf';
 
-const PORT_DEV_WEBPACK = 8000;
-const PORT_DEV_EXPRESS = 8080;
-
-const argv = minimist(process.argv.slice(2));
-
-let config;
+console.log('0');
 
 try {
-  config = require(`./config/${argv.config}`);
-} catch (err) {
-  config = {};
-}
+  const PORT_DEV_WEBPACK = 8000;
+  const PORT_DEV_EXPRESS = 8080;
 
-if (argv.dev === true) {
-  const compiler = webpack(webpackConfig({
-    configName: argv.config,
-    config: config,
-    port: PORT_DEV_WEBPACK,
-    outputPath: __dirname,
-  }));
+  const argv = minimist(process.argv.slice(2));
 
-  compiler.apply(new ProgressPlugin((percentage, msg) => {
-    console.log(`${Math.floor(percentage * 100)}%`, msg);
-  }));
+  let config;
 
-  const server = new WebpackDevServer(compiler, {
-    // webpack-dev-server options
-    hot: true,
-    historyApiFallback: true,
-
-    // webpack-dev-middleware options
-    quiet: false,
-    noInfo: false,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-    },
-    stats: {
-      modules: false,
-      chunks: false,
-      chunkModules: false,
-      colors: true,
-    },
-    contentBase: './server/public',
-    proxy: {
-      '*': `http://local.splitme.net:${PORT_DEV_EXPRESS}`,
-    },
-  });
-
-  server.listen(PORT_DEV_WEBPACK, undefined, () => {});
-} else {
-  let outputPath;
-
-  if (config.platform === 'android') {
-    outputPath = 'cordova/www';
-  } else if (config.platform === 'browser') {
-    outputPath = 'server/static';
-  } else {
-    outputPath = 'server/local';
+  try {
+    config = require(`./config/${argv.config}`);
+  } catch (err) {
+    config = {};
   }
 
-  rimraf.sync(`${outputPath}`);
-
-  const compiler = webpack(webpackConfig({
-    configName: argv.config,
-    config: config,
-    outputPath: outputPath,
-  }));
-
-  compiler.apply(new ProgressPlugin((percentage, msg) => {
-    console.log(`${Math.floor(percentage * 100)}%`, msg);
-  }));
-
-  compiler.run((err, stats) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    console.log(stats.toString({
-      colors: true,
-      hash: false,
-      timings: false,
-      assets: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false,
-      children: true,
+  if (argv.dev === true) {
+    const compiler = webpack(webpackConfig({
+      configName: argv.config,
+      config: config,
+      port: PORT_DEV_WEBPACK,
+      outputPath: __dirname,
     }));
 
-    if (stats.hasErrors()) {
-      process.exit(1);
+    compiler.apply(new ProgressPlugin((percentage, msg) => {
+      console.log(`${Math.floor(percentage * 100)}%`, msg);
+    }));
+
+    const server = new WebpackDevServer(compiler, {
+      // webpack-dev-server options
+      hot: true,
+      historyApiFallback: true,
+
+      // webpack-dev-middleware options
+      quiet: false,
+      noInfo: false,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000,
+      },
+      stats: {
+        modules: false,
+        chunks: false,
+        chunkModules: false,
+        colors: true,
+      },
+      contentBase: './server/public',
+      proxy: {
+        '*': `http://local.splitme.net:${PORT_DEV_EXPRESS}`,
+      },
+    });
+
+    server.listen(PORT_DEV_WEBPACK, undefined, () => {});
+  } else {
+    let outputPath;
+
+    if (config.platform === 'android') {
+      outputPath = 'cordova/www';
+    } else if (config.platform === 'browser') {
+      outputPath = 'server/static';
     } else {
-      process.exit(0);
+      outputPath = 'server/local';
     }
-  });
+
+    rimraf.sync(`${outputPath}`);
+
+    const compiler = webpack(webpackConfig({
+      configName: argv.config,
+      config: config,
+      outputPath: outputPath,
+    }));
+
+    compiler.apply(new ProgressPlugin((percentage, msg) => {
+      console.log(`${Math.floor(percentage * 100)}%`, msg);
+    }));
+
+    compiler.run((err, stats) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      console.log(stats.toString({
+        colors: true,
+        hash: false,
+        timings: false,
+        assets: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false,
+        children: true,
+      }));
+
+      if (stats.hasErrors()) {
+        process.exit(1);
+      } else {
+        process.exit(0);
+      }
+    });
+  }
+} catch (e) {
+  throw e;
 }
