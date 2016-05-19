085e8270b96349523daaaa72bc8e8c786514f77e
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-05-19 20:33:06 +0200

    [i18n] Upgrade i18n-extract and some npm modules

--- i18n-tasks.js
+++ i18n-tasks.js
@@ -3,6 +3,7 @@ import {
   findMissing,
   findUnused,
   findDuplicated,
+  forbidDynamic,
   flatten,
 } from 'i18n-extract';
 import frLocale from './src/locale/frLocale';
@@ -19,6 +20,7 @@ let reports = [];
 reports = reports.concat(findMissing(frLocaleFlattened, keys));
 reports = reports.concat(findUnused(frLocaleFlattened, keys));
 reports = reports.concat(findDuplicated(frLocaleFlattened, keys));
+reports = reports.concat(forbidDynamic(frLocaleFlattened, keys));
 
 if (reports.length > 0) {
   console.log(reports); // eslint-disable-line no-console
