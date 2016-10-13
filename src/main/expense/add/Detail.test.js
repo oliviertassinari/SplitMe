8c7f73bd3ba99e35462538498399d5cbc0737251
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-10-13 22:30:00 +0200

    [test] Add a new one for ExpenseDetail

---
+++ src/main/expense/add/Detail.test.js
@@ -0,0 +1,29 @@
+// @flow weak
+/* eslint-env mocha */
+
+import React from 'react';
+import Immutable from 'immutable';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import DatePicker from 'material-ui-build/src/DatePicker';
+import { ExpenseDetail } from './Detail';
+
+describe('<ExpenseDetail />', () => {
+  describe('DatePicker', () => {
+    it('should format the date correctly', () => {
+      const wrapper = shallow(
+        <ExpenseDetail
+          account={Immutable.fromJS({})}
+          accounts={Immutable.fromJS([{}])}
+          dispatch={() => {}}
+          expense={Immutable.fromJS({})}
+          screenDialog=""
+        />
+      );
+
+      assert.strictEqual(wrapper.find(DatePicker).length, 1);
+      assert.strictEqual(wrapper.instance().formatDate(new Date(2013, 9, 23)),
+        'Wednesday, October 23, 2013');
+    });
+  });
+});
